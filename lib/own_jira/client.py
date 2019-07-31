bad35a1563c3f75f34a95dccdb00b5cfb82e41a8
oleksandr <oleksandr.mateik@rescrypto.pro>
2019-08-01 01:07:56 +0300

    jira_recorder

---
+++ lib/own_jira/client.py
@@ -0,0 +1,44 @@
+import warnings
+
+from jira.client import JIRA, Issue, translate_resource_args, ResultList
+from jira.resources import Board, GreenHopperResource
+
+
+class JIRA(JIRA):
+    def issues_by_board(self, board_id):
+        # todo remove replace
+        r_json = self._get_json('board/%s/issue/' % board_id,
+                                base=self.AGILE_BASE_URL.replace('{agile_rest_path}', 'agile'))
+        return [Issue(self._options, self._session, raw_issues_json) for raw_issues_json in r_json['issues']]
+
+    @translate_resource_args
+    def boards(self, startAt=0, maxResults=50, type=None, name=None, boardKeyOrId=None):
+        """Get a list of board resources.
+
+        :param startAt: The starting index of the returned boards. Base index: 0.
+        :param maxResults: The maximum number of boards to return per page. Default: 50
+        :param type: Filters results to boards of the specified type. Valid values: scrum, kanban.
+        :param name: Filters results to boards that match or partially match the specified name.
+        :rtype: ResultList[Board]
+
+        When old GreenHopper private API is used, paging is not enabled and all parameters are ignored.
+        """
+        params = {}
+        if type:
+            params['type'] = type
+        if name:
+            params['name'] = name
+        if boardKeyOrId:
+            params['projectKeyOrId'] = boardKeyOrId
+
+        if self._options['agile_rest_path'] == GreenHopperResource.GREENHOPPER_REST_PATH:
+            # Old, private API did not support pagination, all records were present in response,
+            #   and no parameters were supported.
+            if startAt or maxResults or params:
+                warnings.warn('Old private GreenHopper API is used, all parameters will be ignored.', Warning)
+
+            r_json = self._get_json('rapidviews/list', base=self.AGILE_BASE_URL)
+            boards = [Board(self._options, self._session, raw_boards_json) for raw_boards_json in r_json['views']]
+            return ResultList(boards, 0, len(boards), len(boards), True)
+        else:
+            return self._fetch_pages(Board, 'values', 'board', startAt, maxResults, params, base=self.AGILE_BASE_URL)
--- tes.py
+++ tes.py
@@ -1,52 +1,174 @@
-import jira.client
-from jira.client import JIRA
-import re
-
-options = {'server': 'http://jira.rescrypto.pro/'}
-connect = JIRA(options, basic_auth=('oleksandr.mateik', 'Cvdovdmav89'))
-
-print(connect.projects())
-# print(connect.project(10200))
-issues_in_proj = connect.search_issues('issuetype=Task', maxResults=None)
-# print(issues_in_proj)
-
-issue = connect.issue('BI-45')
-
-summary = issue.fields.summary
-print(summary)
-
-comments_a = issue.fields.comment.comments
-print(comments_a)
-comment = connect.comment('BI-45', '10345')
-#
-capacity = issue.raw['fields']['comment']
-print(capacity)
-comments = []
-for i in capacity['comments']:
-    print(i['body'])
+from googleapiclient.errors import HttpError
+from own_jira.client import JIRA
+import pygsheets
+import datetime
+import maya
 
 
+class GetJiraData:
+    def __init__(self):
+        # connect to own_jira api
+        self.options = {'server': 'http://jira.rescrypto.pro/'}
+        self.jira_connect = JIRA(self.options, basic_auth=('oleksandr.mateik', 'Cvdovdmav89'))
+
+        # connect to google sheets
+        self.client = pygsheets.authorize('/home/sanya/PycharmProjects/some_task/credentials.json')
+        self.sheet = self.client.open_by_key('1a5eJDqckUPqrwI0ILMKsyuRInstHArjNo2nqlljV4JE')
+
+        # self.worksheet = self.sheet.worksheet(property='title', value='Лист1')
+        self.record_place = None
+        self.type_of_issue = None
+        self.priority = None
+        self.issue_description = None
+        self.name_of_project = None
+        self.add_tab_sheet = None
+        self.count_missing_rows = 2
+
+    def create_data_sheets(self):
+        while True:
+            try:
+                project_name_or_id = input('input id or key of project  ')
+                jira_project = self.jira_connect.project(project_name_or_id)
+
+                # get boards list by project and print it
+                boards = self.jira_connect.boards()
+                for i in boards:
+                    print('Board: name - {}, id - {}'.format(i.name, i.id))
+
+                # checking if board in project
+                while True:
+                    board_id = int(input('Input id board '))
+                    matches = list(filter(lambda board: board.id == board_id, boards))
+                    if matches:
+                        break
+
+                # choose board id and record issues to spreadsheet
+                issues = self.jira_connect.issues_by_board(board_id)
+
+                # filter issues by type --> or view all issues
+                while True:
+                    choose_type = input('choose the type or press "rec" to record all issues ')
+                    if choose_type == "rec":
+                        break
+                    else:
+                        issues = list(
+                            filter(lambda type_issue: type_issue.fields.issuetype.name == choose_type, issues))
+                        break
+
+                while True:
+                    choose_period = input('Do you want to record for the particular period?  press Y/N  ').lower()
+                    if choose_period == 'n' and not 'y':
+                        break
+                    else:
+                        start_from = input('Enter from date 2019-07-01 - ')
+                        end_to = input('Enter to date 2019-07-04 - ')
+                        issues = list(
+                            filter(lambda date_issue: start_from <= maya.parse(
+                                date_issue.fields.created).date.isoformat() <= end_to, issues))
+                    break
+
+                # record name of project
+                self.add_tab_sheet = self.sheet.add_worksheet(
+                    '; '.join([jira_project.name, matches[0].name, str(datetime.datetime.now())])
+                )
+
+                # headers for spreadsheet
+                fields = [
+                    {
+                        'name': 'summary',
+                        'default': 'noname',
+                    },
+                    {
+                        'name': 'priority',
+                        'default': 'noname',
+                    },
+                    {
+                        'name': 'issuetype',
+                        'default': 'noname',
+                    },
+                    {
+                        'name': 'description',
+                        'default': 'noname',
+                    },
+                    {
+                        'name': 'assignee.name',
+                        'default': 'noname',
+                    },
+                    {
+                        'name': 'timespent',
+                        'default': 'noname',
+                    },
+                    {
+                        'name': 'aggregatetimespent',
+                        'default': 'noname',
+                    },
+                    {
+                        'name': 'resolution',
+                        'default': 'noname',
+                    },
+                    {
+                        'name': 'resolutiondate',
+                        'default': 'noname',
+                    },
+                    {
+                        'name': 'labels',
+                        'default': 'noname',
+                    },
+                    {
+                        'name': 'timeestimate',
+                        'default': 'noname',
+                    },
+                    {
+                        'name': 'aggregatetimeoriginalestimate',
+                        'default': 'noname',
+                    },
+                    {
+                        'name': 'timeoriginalestimate',
+                        'default': 'noname',
+                    },
+                    {
+                        'name': 'status',
+                        'default': 'noname',
+                    },
+                    {
+                        'name': 'subtasks',
+                        'default': 'noname',
+                    },
+
+                ]
+
+                # set headers for each column
+                for column_index, value in enumerate(fields):
+                    self.record_place = pygsheets.Cell(pos=(1, 1 + column_index),
+                                                       worksheet=self.add_tab_sheet)
+                    if '.' in value['name']:
+                        self.record_place.value = str(value['name'].split('.')[0])
+                    else:
+                        self.record_place.value = str(value['name'])
+
+                # set data for each issue
+                for count, issue in enumerate(issues):
+                    for column_index, field in enumerate(fields):
+                        self.record_place = pygsheets.Cell(pos=(self.count_missing_rows + count, column_index + 1),
+                                                           worksheet=self.add_tab_sheet)
+                        value = ''
+                        if '.' in field['name']:
+                            if 'assignee' in field['name']:
+                                value = issue.fields.assignee.name
+                        elif 'subtasks' in field['name']:
+                            value = len(issue.fields.subtasks)
+                        elif 'labels' in field['name']:
+                            value = ', '.join(list([label for label in issue.fields.labels]))
+                        else:
+                            value = getattr(issue.fields, field['name'], field['default'])
+                        self.record_place.value = str(value)
+
+            except HttpError:
+                print("Probably you have already create tab of spreadsheet")
+
+            except Exception:
+                print("You've entered incorrect id or name of project")
 
 
-# connect.assign_issue(issue, )
-
-# for issue in connect.search_issues('reporter = currentUser() order by created desc', maxResults=3):
-#     print('{}: {} -- {}'.format(issue.key, issue.fields.summary, issue.fields.description))
-
-# issue.update(
-#     summary="Test done", description='Changed the summary to be different.')
-
-# new_issue = connect.create_issue(
-# project='10200', summary='Test',description='researching working with jira api', issuetype={'name': 'Task'}
-# )
-
-#OR
-# issue_dict = {
-#     'project': {'id': 123},
-#     'summary': 'New issue from jira-python',
-#     'description': 'Look into this one',
-#     'issuetype': {'name': 'Bug'},
-# }
-# new_issue = jira.create_issue(fields=issue_dict)
-
-# connect.add_comment(issue, 'Done')
+runner = GetJiraData()
+runner.create_data_sheets()
